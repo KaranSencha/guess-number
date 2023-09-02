df1b9cb389744fb36e6d4b00e42fc691180db48d
Karan Sencha <karansencha327@gmail.com>
2023-09-02 10:04:19 +0530

    Make responsive for all divices

--- script.js
+++ script.js
@@ -7,54 +7,59 @@ let highscore = 0;
 const displayMessage = function (message) {
   document.querySelector(".message").textContent = message;
 };
+const checkButton = document.querySelector(".check");
 
-document.querySelector(".check").addEventListener("click", function () {
-	const guess = Number(document.querySelector(".guess").value);
-	console.log(guess, typeof guess);
+const inputValue = document.querySelector(".guess");
+inputValue.addEventListener("keydown", function (event) {
 
-	// When there is no input
-	if (!guess) {
-		displayMessage("⛔️ No number!");
-
-		// When player wins
-	} else if (guess === secretNumber) {
-		displayMessage("🎉 Correct Number!");
-		document.querySelector(".number").textContent = secretNumber;
-
-		document.querySelector("body").style.backgroundColor = "#60b347";
-		document.querySelector(".number").style.width = "30rem";
-
-		if (score > highscore) {
-			highscore = score;
-			document.querySelector(".highscore").textContent = highscore;
-		}
-
-		// When guess is wrong
-	} else if (guess !== secretNumber) {
-		if (score > 1) {
-			displayMessage(guess > secretNumber ? "📈 Too high!" : "📉 Too low!");
-			score--;
-			document.querySelector(".score").textContent = score;
-		} else {
-
-			displayMessage("💥 You lost the game!");
-			document.querySelector(".score").textContent = 0;
-		}
+	if (event.keyCode === 13) {
+		event.preventDefault();
+		checkButton.click();
 	}
+})
 
+checkButton.addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
 
-	document.querySelector(".again").addEventListener("click", function () {
-		score = 20;
-		secretNumber = Math.trunc(Math.random() * 20) + 1;
+  // When there is no input
+  if (!guess) {
+    displayMessage("⛔️ No number!");
 
-		// document.querySelector('.message').textContent = 'Start guessing...';
-		displayMessage("Start guessing...");
-		document.querySelector(".score").textContent = score;
-		document.querySelector(".number").textContent = "?";
-		document.querySelector(".guess").value = "";
+    // When player wins
+  } else if (guess === secretNumber) {
+    displayMessage("🎉 Correct Number!");
+    document.querySelector(".number").textContent = secretNumber;
 
-		document.querySelector("body").style.backgroundColor = "#222";
-		document.querySelector(".number").style.width = "15rem";
-	});
+    document.querySelector("body").style.backgroundColor = "#198754";
 
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
+
+    // When guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? "📈 Too high!" : "📉 Too low!");
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      displayMessage("💥 You lost the game!");
+      document.querySelector(".score").textContent = 0;
+    }
+  }
+
+  document.querySelector(".again").addEventListener("click", function () {
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+    // document.querySelector('.message').textContent = 'Start guessing...';
+    displayMessage("Start guessing...");
+    document.querySelector(".score").textContent = score;
+    document.querySelector(".number").textContent = "?";
+    document.querySelector(".guess").value = "";
+
+    document.querySelector("body").style.backgroundColor = "#222";
+    document.querySelector(".number").style.width = "15rem";
+  });
 });
